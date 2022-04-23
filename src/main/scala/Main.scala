6d93de3d521462582572935de3e60a8bf4bdc4eb
Riley Baker <vishnukey@gmail.com>
2022-04-22 19:32:29 -0600

    Basic discord both using Javadcord library

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,20 @@
-@main def hello: Unit = 
+import org.javacord.api.DiscordApiBuilder
+import org.javacord.api.interaction.SlashCommand
+
+import scala.io.Source
+
+@main def hello: Unit = {
+
+  val token = Source.fromResource("token.txt").getLines().nextOption() match {
+      case Some(text) => text
+      case None => throw Exception("No token found")
+    }
+
+  val api = DiscordApiBuilder().setToken(token).login().join()
+  SlashCommand.`with`("ping", "a simple ping pong command!").createGlobal(api).join()
+
   println("Hello world!")
   println(msg)
+}
 
 def msg = "I was compiled by Scala 3. :)"
